ff62227fd5dc5b5bdbc3bb10922c41b0af959db2
Armand1711 <125480832+Armand1711@users.noreply.github.com>
2025-08-06 01:50:31 +0200

    Add firebaseConfig.js for Firebase setup.

---
+++ FairwayConnect/src/firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAVZc1L9BxaUYOx_XCaFNfGeCUD1fr7WsE",
+  authDomain: "fairwayconnect-2025.firebaseapp.com",
+  databaseURL: "https://fairwayconnect-2025-default-rtdb.firebaseio.com",
+  projectId: "fairwayconnect-2025",
+  storageBucket: "fairwayconnect-2025.firebasestorage.app",
+  messagingSenderId: "657734590072",
+  appId: "1:657734590072:web:55f0865831e6abc460978d"
+};
